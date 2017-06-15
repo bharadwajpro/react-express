5791a6e621b3820175af99e038996b53b88b8ae5
Krishna Bharadwaj <bharadwaj6598@gmail.com>
2017-06-15 12:54:18 +0530

    boilerplate ready

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(process.env.PORT || 8080, function () {
+    console.log('Server listening on port', process.env.PORT || 8080);
+});
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require("webpack");
+var debug = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  context: __dirname+'/src',
+  devtool: debug ? "inline-sourcemap" : false,
+  entry: "./js/scripts.js",
+  output: {
+    path: __dirname+'/public/',
+    filename: "scripts.min.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+        }
+      }
+    ]
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ]
+};
